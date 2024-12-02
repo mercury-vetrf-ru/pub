657c10b11a3082b89119372eeb79e7b8d1880b68
Alexandr Bondarenko <mercury.vetrf.ru@gmail.com>
2024-12-02 05:33:23 +0300

    search

--- generate_uuid.py
+++ generate_uuid.py
@@ -7,7 +7,7 @@ def generate_uuid():
         # Допустимые символы: 0-9, a-f, A-F
         chars = string.hexdigits
         return (''.join(random.choice(chars) for _ in range(4))).upper()
-    
+
     # Генерируем 8 блоков и соединяем их дефисами
     blocks = [generate_block() for _ in range(8)]
     return '-'.join(blocks)
